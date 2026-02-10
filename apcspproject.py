522774938c32fe599452cde2cf03df295ef2527c
jwssimmons1-crypto <jws.simmons1@gmail.com>
2026-02-10 13:51:20 -0800

    Add files via upload

---
+++ apcspproject.py
@@ -0,0 +1,242 @@
+import turtle as trtl
+x= trtl.Turtle()
+
+# create background of image
+x.penup()
+x.goto(-510,430)
+x.speed(0)
+x.pendown()
+x.begin_fill()
+x.fillcolor("#99E1F1")
+for step in range(4):
+    x.forward(1012)
+    x.right(90)
+x.end_fill()
+
+
+# create base of house
+x.penup()
+x.goto(0,-200)
+x.begin_fill()
+x.fillcolor("blue")
+x.pendown()
+x.forward(300)
+x.left(90)
+x.forward(200)
+x.left(90)
+x.forward(300)
+x.end_fill()
+
+# create roof of house
+x.penup()
+x.goto(0,0)
+x.begin_fill()
+x.fillcolor("red")
+x.pendown()
+x.forward(50)
+
+x.penup()
+x.goto(350,0)
+x.pendown()
+x.forward(50)
+x.right(135)
+
+x.penup()
+x.goto(-50,0)
+
+x.pendown()
+x.forward(282)
+x.right(90)
+x.forward(282)
+x.end_fill()
+
+# create door of house
+x.penup()
+x.goto(175,-200)
+x.begin_fill()
+x.fillcolor("red")
+x.pendown()
+x.left(135)
+x.forward(140)
+x.left(90)
+x.forward(50)
+x.left(90)
+x.forward(140)
+x.end_fill()
+
+# create circle window of house
+x.penup()
+x.goto(40, -85)
+x.begin_fill()
+x.fillcolor("pink")
+x.pendown()
+x.circle(30)
+x.end_fill()
+
+# create triangle window of house
+x.penup()
+x.goto(260,-110)
+x.begin_fill()
+x.fillcolor("white")
+x.pendown()
+x.right(90)
+for step in range(3):
+    x.forward(60)
+    x.right(120)
+x.end_fill()
+
+# create chimney of house
+x.penup()
+x.goto(230,118)
+x.begin_fill()
+x.fillcolor("#1CDAD4")
+x.pendown()
+x.right(90)
+x.forward(70)
+x.right(90)
+x.forward(30)
+x.right(90)
+x.forward(100)
+x.end_fill()
+
+# add grass to the ground
+x.penup()
+x.goto(-510,-200)
+x.begin_fill()
+x.fillcolor("#22A90D")
+x.pendown()
+x.left(90)
+x.forward(1012)
+x.right(90)
+x.forward(250)
+x.right(90)
+x.forward(1012)
+x.end_fill()
+
+# create tree trunk
+x.penup()
+x.goto(-280,-200)
+x.begin_fill()
+x.fillcolor("sienna")
+x.pendown()
+x.right(90)
+x.forward(110)
+x.right(90)
+x.forward(40)
+x.right(90)
+x.forward(110)
+x.end_fill()
+
+# create tree leaves
+x.penup()
+x.goto(-260, 10)
+x.begin_fill()
+x.fillcolor("#1CD41F")
+x.pendown()
+
+r = 0
+
+for step in range(6):
+    x.setheading(r)
+    x.circle(60)
+    r = r + 60
+x.end_fill()
+
+# create cup on tree leaves
+x.penup()
+x.goto(-305, -20)
+x.begin_fill()
+x.fillcolor("#64A2E0")
+x.pendown()
+x.circle(50)
+x.end_fill()
+
+x.penup()
+x.goto(-315, -10)
+x.begin_fill()
+x.fillcolor("#64A2E0")
+x.pendown()
+x.circle(50)
+x.end_fill()
+
+# create Sun 
+
+horiz_turtles = [] 
+
+turtle_shapes = ["circle"]
+horiz_colors = ["yellow"]
+
+tloc = 50
+for s in turtle_shapes:
+
+  ht = trtl.Turtle(shape=s)
+  horiz_turtles.append(ht)
+  ht.penup()
+  new_color = horiz_colors.pop()
+  ht.fillcolor(new_color)
+  ht.goto(380,320)
+  ht.shapesize(8)
+  ht.setheading(0)
+  
+for step in range(50):
+    for ht in horiz_turtles:
+        ht.backward(9)
+        
+
+        if step ==0:
+           collision_shape = turtle_shapes.pop() if turtle_shapes else "classic"
+           collision_color = "yellow"
+
+# create eyes on tree
+user_name = input("What is your name? ")
+print("Hi", user_name)
+answer = input("Would you like to add some details to the tree? State 'yes' or 'no'. ")
+if answer == "yes": 
+    print("Ok", user_name, "let's get started")
+    eyes = input("Do you want to add eyes? State 'yes' or 'no'. ")
+    if eyes == "yes":
+        x.penup()
+        x.goto(-295,25)
+        x.begin_fill()
+        x.fillcolor("black")
+        x.pendown()
+        x.circle(10)
+        x.end_fill()
+        
+        x.penup()
+        x.goto(-265,25)
+        x.begin_fill()
+        x.fillcolor("black")
+        x.pendown()
+        x.circle(10)
+        x.end_fill()
+        x.penup()
+        x.goto(-1000,1000)
+    if eyes == "no": 
+        print("Ok I will not add eyes. Have a good day! ")
+
+# create smile/frown on tree
+    expression = input("Do you want to add a frown? State 'frown' or 'no'. ")
+    if expression == "frown":
+        x.penup()
+        x.pensize(5)
+        x.goto(-295,-5)
+        x.begin_fill()
+        x.fillcolor("black")
+        x.pendown()
+        x.setheading(0)
+        x.forward(50)
+        x.end_fill()
+        x.penup()
+        x.goto(-1000,1000)
+    if expression == "no":
+        print("Ah dang alright. Byeeeee! ")
+
+if answer == "no":
+    print("That's the end of the painting. Bye! ")
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
